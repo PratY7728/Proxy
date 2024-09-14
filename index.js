686fa4996e47d88523b9e85b60f93a2f1529a33a
PratY7728 <152558066+PratY7728@users.noreply.github.com>
2024-09-14 15:01:15 +0530

    try now

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const targetUrl = 'https://api.backpack.exchange';
 
 // Handle CORS
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://frontend-exchange-xi.vercel.app');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
